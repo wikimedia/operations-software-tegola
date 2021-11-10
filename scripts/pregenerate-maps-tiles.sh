b3bea8f52c680b21718edcc29af99b37daf19f1c
Yiannis Giannelos <jgiannelos@wikimedia.org>
2021-11-10 16:52:35 +0100

    tile-pregeneration: Force overwriting tiles on cache operations
    By default tegola skips seeding/purging existing tiles in cache.
    In our case we want to override this for tile invalidation.
    
    Change-Id: Ib4631fb34d165af7a5e5bfb380ba6852d90f6c15

--- scripts/pregenerate-maps-tiles.sh
+++ scripts/pregenerate-maps-tiles.sh
@@ -68,7 +68,7 @@ do
 
     # Pregenerate tiles that exist in tilelist
     set -e
-    $TEGOLA_PATH --config "$TEGOLA_CONFIG_PATH" cache $CACHE_OPERATION tile-list "$TILELIST_PATH"
+    $TEGOLA_PATH --config "$TEGOLA_CONFIG_PATH" cache $CACHE_OPERATION --overwrite tile-list "$TILELIST_PATH"
     set +e
 
     if [ "$dequeueStatus" -eq 100 ]  # Queue is empty
